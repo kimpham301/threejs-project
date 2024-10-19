0633d4b51fde4c06f0cbb5be8d2b058c368b5a36
Kim Pham <kim_pham@shi.com>
2024-10-18 22:00:24 -0500

    initial project

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+export default {
+    root: 'src/', // Sources files (typically where index.html is)
+    publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
+
+    build:
+    {
+        outDir: '../dist', // Output in the dist/ folder
+        emptyOutDir: true, // Empty the folder first
+        sourcemap: true // Add sourcemap
+    },
+}
